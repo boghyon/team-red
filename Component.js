71fb74a3e9ea4ff492a380941cb7edbc99f94cde
boghyon <boghyon+github@gmail.com>
2020-02-02 22:02:31 +0100

    Added MVC

--- Component.js
+++ Component.js
@@ -1,11 +1,17 @@
 sap.ui.define([
-  "sap/ui/core/UIComponent"
-], function(UIComponent) {
+  "sap/ui/core/UIComponent",
+  "sap/ui/Device",
+], function(UIComponent, Device) {
   "use strict";
 
   return UIComponent.extend("demo.Component", {
     metadata: {
       manifest: "json"
+    },
+
+    init: function() {
+      UIComponent.prototype.init.apply(this, arguments);
+      this.getModel("device").setData(Device);
     },
 
   });
