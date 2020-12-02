e5ac3ea0623688c6c6cfbd4bc04670642cf8af54
boghyon <boghyon+github@gmail.com>
2020-12-02 22:10:38 +0100

    Improved stuff

--- controller/Master.controller.js
+++ controller/Master.controller.js
@@ -1,13 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-], function(Controller) {
-    "use strict";
+	"sap/ui/core/mvc/Controller",
+], function (Controller) {
+	"use strict";
 
-    return Controller.extend("demo.controller.Master", {
-        navToDetailOf: async function(clickedItem) {
-            const rootView = await this.getOwnerComponent().getRootControl().loaded();
-            const container = rootView.byId("flexibleColumnLayout");
-            container.setLayout("TwoColumnsMidExpanded");
-        },
-    });
+	return Controller.extend("demo.controller.Master", {
+		navToDetailOf: async function (clickedItem) {
+			const rootView = await this.getOwnerComponent().getRootControl().loaded();
+			const container = rootView.byId("flexibleColumnLayout");
+			container.setLayout("TwoColumnsMidExpanded");
+		},
+	});
 });
